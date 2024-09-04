a99e68d2a78d3cf83d2e6d8b68b0b4c546e2c2a1
TheVegeta <iamvegeta@protonmail.com>
2024-09-04 11:32:19 +0530

    fix expo font

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -5,23 +5,31 @@ import {
 import { NavigationContainer } from "@react-navigation/native";
 import { TamaguiProvider, Theme, View } from "@tamagui/core";
 import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React, { FC, ReactNode } from "react";
+import { FC, ReactNode } from "react";
 import { StatusBar, useColorScheme } from "react-native";
 import { PortalProvider } from "tamagui";
 import tamaguiConfig from "../tamagui.config";
+import { useRunAfterInteraction } from "./hooks/useRunAfterInteraction";
 import { useAppStore } from "./store";
 
 const AppProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const colorScheme = useColorScheme();
   const theme = useAppStore((state) => state.theme);
 
-  const [loaded] = useFonts({
+  const [loaded, error] = useFonts({
     Inter: Poppins_400Regular,
     InterBold: Poppins_700Bold,
   });
 
-  if (!loaded) return null;
+  useRunAfterInteraction(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) return null;
 
   return (
     <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
